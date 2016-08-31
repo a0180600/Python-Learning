2af62c206974b80d22ced876a6e4260f0f5299ff
a0180600 <505128489@qq.com>
2016-08-31 20:27:27 +0800

    +str_format.py

---
+++ a-byte-of-python/str_format.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# FileName : str_format.py
+age = 22
+name = 'SevenJoy'
+print ('{0} is {1} years old'.format(name, age))
+print ('Why is {0} playing with thatt python?'.format(name))
